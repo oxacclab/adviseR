1344537bb4c7e55163a4d12ba186765c948b5e8f
mjaquiery <matt.jaquiery@psy.ox.ac.uk>
2020-10-08 10:40:47 +0100

    Tests to improve code coverage

--- tests/testthat/test-basic.R
+++ tests/testthat/test-basic.R
@@ -16,3 +16,18 @@ test_that('Simple simulations', {
   # expect_identical(model$model, basic.model$model)
   expect_equal(typeof(model), typeof(basic.model))
 })
+
+test_that('Simulation network graphs', {
+  load('data/basic-model.rda')
+  expect_invisible(networkGraph(basic.model))
+})
+
+test_that('Bias graph', {
+  load('data/basic-model.rda')
+  expect_equal('ggplot' %in% class(biasGraph(basic.model)), T)
+})
+
+test_that('Sensitivity graph', {
+  load('data/basic-model.rda')
+  expect_equal('ggplot' %in% class(sensitivityGraph(basic.model)), T)
+})
